c5ef24cef5ef4602c21da0e7eb2c84189c78b4c4
putrimardita38-web <putrimardita38@gmail.com>
2026-05-22 14:15:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 st.title("cesil cantik banget")
-st.title("_Streamlit_ is :blue[cool] :sunglasses:")
+st.title("cantik banget banget")
